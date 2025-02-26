d3c013db7996d8238eb42ded09b0ed574db79071
adamusking <adamuskin@icloud.com>
2025-02-26 16:55:35 +0100

    pridal som nahodne generovanie rychlosti

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ while True:
     CH4 = round(random.uniform(1.8, 3.5), 2)
     N2O = round(random.uniform(0.2, 1.5), 2)
     S2O = round(random.uniform(0, 20), 2)
+    speed = round(random.uniform(8, 11), 2)
     latitude = 48.6690
     longitude = 19.6990
     battery = round(random.uniform(0, 100), 2)
@@ -32,6 +33,7 @@ while True:
         .field("pressure", pressure)
         .field("CO2", CO2)
         .field("CO", CO)
+        .field("speed", speed)
         .field("CH4", CH4)
         .field("N2O", N2O)
         .field("S2O", S2O)
@@ -43,6 +45,6 @@ while True:
 
     print(f'Written point: Temperature: {temperature}, Altitude: {altitude}, Humidity: {humidity}, '
           f'Pressure: {pressure}, CO2: {CO2}, CO: {CO}, CH4: {CH4}, N2O: {N2O}, S2O: {S2O}, '
-          f'Latitude: {latitude}, Longitude: {longitude}, Battery: {battery}')
+          f'Latitude: {latitude}, Longitude: {longitude}, Battery: {battery}, Speed: {speed}')
     write_api.write(bucket=bucket, org=org, record=point)
     time.sleep(1)
